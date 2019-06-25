1ef017984cc824663b7885e5cc4d5d13859b0c6c
Trond Bo <trbo12@gmail.com>
2019-06-25 20:52:36 +0000

    delete my tash

---
+++ delete_my_trash.js
@@ -0,0 +1,13 @@
+function delete_my_gmail_trash()
+{
+  var threads = GmailApp.search( "in:trash", 501, 500 );
+  Logger.log(threads.length ); 
+  return ;
+  for ( var i = 0; i < threads.length; i++ ) {
+      num_of_messages_in_the_thread = threads[i].getMessageCount();
+      for ( var j = 0; j < num_of_messages_in_the_thread; j++ ) {
+           Gmail.Users.Messages.batchDelete( {  "ids": [ threads[i].getMessages()[j].getId() ]}, "me" );
+      }
+  }
+}
+
